841cd7afcc861f914ac9ff192aa76b5a3add7582
vickysehgal <vk1512694@gmail.com>
2023-02-04 19:34:46 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// switch on function
+
+function bulbOn(){
+    let bulb = document.querySelector(".bulbOff");
+    bulb.setAttribute("src", "images/bulb_on.png");
+}
+
+
+function bulbOff(){
+    let bulb = document.querySelector(".bulbOff");
+    bulb.setAttribute("src", "images/bulb_off.png");
+}
